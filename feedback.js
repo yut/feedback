9a3f9d4652364dcab70e4a0b3f188f40ed3ba26e
Greg Yut <greg@sharedcompass.com>
2022-02-23 16:37:17 -0500

    Remove warning by adding semicolons

--- feedback.js
+++ feedback.js
@@ -17,25 +17,25 @@ function setCookie(name, value, expiresInDays, secure, sameSite) {
 
 function updateCookie(name, value) {
   // Example: updateCookie("cookie name", "new cookie value");
-  document.cookie = `${name}=${value}`
+  document.cookie = `${name}=${value}`;
 }
 
 function getCookie(name) {
   // Example: getCookie("cookie name")
-  var namePattern = `${name}=`
+  var namePattern = `${name}=`;
   var cookies = decodeURIComponent(document.cookie).split(";");
   for (var cookie, index = 0; index < cookies.length; index++) {
     cookie = cookies[index];
     if (cookie.charAt(0) == " ") cookie = cookie.substring(1);
     if (cookie.indexOf(namePattern) == 0) return cookie.substring(namePattern.length, cookie.length)
   }
-  return null
+  return null;
 }
 
 function hex4() {
   // Returns a random, 4-digit hexadecimal number.
   // 2**16 = 65536. Bitwise OR with 0 floors number, converting float to int.
-  return (65536 * (1 + Math.random()) | 0).toString(16).substring(1)
+  return (65536 * (1 + Math.random()) | 0).toString(16).substring(1);
 }
 
 function makeUuid() {
@@ -44,7 +44,7 @@ function makeUuid() {
           hex4() + "-" +
           "4" + hex4().substr(0, 3) + "-" +
           hex4() + "-" +
-          hex4() + hex4() + hex4()).toLowerCase()
+          hex4() + hex4() + hex4()).toLowerCase();
 }
 
 function overrideAttributes(targetObject) {
@@ -67,7 +67,7 @@ function overrideAttributes(targetObject) {
       }
     }
   }
-  return targetObject
+  return targetObject;
 }
 
 (function() {
@@ -100,11 +100,11 @@ function overrideAttributes(targetObject) {
         }
         // Save the uuid in the cookies.
         setCookie("_feedback_uuid", Feedback._sourceUuid, 90, true, "None");
-        setCookie("_feedback_uuid_lax", Feedback._sourceUuid, 90, false, "Lax")
+        setCookie("_feedback_uuid_lax", Feedback._sourceUuid, 90, false, "Lax");
         return Feedback._sourceUuid;
       }
     },
-  }
+  };
 }());
 
 (function() {
@@ -114,7 +114,7 @@ function overrideAttributes(targetObject) {
     useSendBeacon: true,
     useGetParams: true,
     capture: function(attributes) {
-      for (var key in attributes) Feedback.parcel.collector.push([key, attributes[key]])
+      for (var key in attributes) Feedback.parcel.collector.push([key, attributes[key]]);
     },
     send: function() {
       // Note: An empty Feedback.parcel.collector gets logged, but no send is attempted.
@@ -123,19 +123,19 @@ function overrideAttributes(targetObject) {
         var index, pair, results = {};
         for (index in Feedback.parcel.collector) {
           if ("function" != typeof(pair = Feedback.parcel.collector[index])) {
-            key = pair[0]
-            value = pair[1]
+            key = pair[0];
+            value = pair[1];
             // Case 1: The results hash doesn't have this key, so add it.
             if (typeof results[key] == "undefined") {
-              results[key] = value
+              results[key] = value;
 
               // Case 2: The results value is an array, so push value onto the array.
             } else if (Object.prototype.toString.call(results[key]) === "[object Array]") {
-              results[key].push(value)
+              results[key].push(value);
 
               // Case 3: The results value is not a collection, so make it an array of both the existing and new values.
             } else {
-              results[key] = [results[key], value]
+              results[key] = [results[key], value];
             }
           }
         }
@@ -151,7 +151,7 @@ function overrideAttributes(targetObject) {
 
           // Send method 1: navigator.sendBeacon.
           if (Feedback.parcel.useSendBeacon && navigator.sendBeacon !== undefined) {
-            Feedback.parcel.useSendBeacon = navigator.sendBeacon(Feedback.destinationUrl(), formData)
+            Feedback.parcel.useSendBeacon = navigator.sendBeacon(Feedback.destinationUrl(), formData);
           }
           // Send method 2: Append params to the GET request.
           else if (Feedback.parcel.useGetParams && JSON.stringify(results).length < 4096) {
@@ -162,27 +162,27 @@ function overrideAttributes(targetObject) {
             image.onerror = function() {
               if (Feedback.dev) {
                 Feedback.parcel.enabled = false;
-                console.warn("Problem sending feedback parcel. Disabling parcel.")
+                console.warn("Problem sending feedback parcel. Disabling parcel.");
               }
-            }
+            };
           }
           // Send method 3: An XHR post request, sending form data.
           else {
             var xhr = new XMLHttpRequest;
             xhr.open("POST", Feedback.destinationUrl(), true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            xhr.send(formData)
+            xhr.send(formData);
             xhr.onerror = function() {
               if (Feedback.dev) {
                 Feedback.parcel.enabled = false;
-                console.warn("Problem sending feedback parcel. Disabling parcel.")
+                console.warn("Problem sending feedback parcel. Disabling parcel.");
               }
-            }
+            };
           }
         }
         // Empty collector since we've already sent them.
-        return Feedback.parcel.collector = []
+        return Feedback.parcel.collector = [];
       }
     }
-  }
+  };
 }());
